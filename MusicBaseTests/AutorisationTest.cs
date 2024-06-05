fbee2997efb8ddcf6601aa5ebdfa07f978fcbb2c
Иван Стасюк <ivanstasyukjob@yandex.ru>
2024-06-05 14:04:19 +1000

    ..

---
+++ MusicBaseTests/AutorisationTest.cs
@@ -0,0 +1,26 @@
+﻿using DiplomAppMusicBase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace MusicBaseTests
+{
+    [TestClass]
+    public class AutorisationTest
+    {
+        [TestMethod]
+        public void Autorisation()
+        {
+            string TBLoginTest = "ivanstasyuk";
+            string TBPasswordTest = "ivanstasyuk";
+            Assert.AreEqual(TBLoginTest, TBPasswordTest);
+        }
+        [TestMethod]
+        public void Autorisation1()
+        {
+            string TBLoginTest = "";
+            string TBPasswordTest = "";
+            Assert.AreEqual(TBLoginTest, TBPasswordTest);
+        }
+    }
+}
---
+++ MusicBaseTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MusicBaseTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicBaseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0083dc9-10ed-4f1f-b5c0-53edd82bf6c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
